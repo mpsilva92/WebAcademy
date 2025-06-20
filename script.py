7a35179cd9dd9717682f72d3824107beb2310c02
mpsilva92 <marccos.praiano@icomp.ufam.edu.br>
2025-06-20 19:04:40 -0300

    Alterando arquivo da nova branch

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
+def subtrair(num1, num2):
+    return (num1 - num2)
+
 
 def somar(num1, num2):
     return (num1 + num2)
@@ -7,4 +10,6 @@ if __name__ == "__main__":
     a = 5
     b = 8
     s = somar(a, b) # funcao para somar
+    sub = subtrair(a, b) # funcao para subtrair
     print(f'Resultado da soma: {s}')
+    print(f'Resultado da subtração eh: {sub}')
