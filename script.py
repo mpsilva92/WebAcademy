b04739b5bc26b51bba1ff72d132c807ebfb3b12a
mpsilva92 <marccos.praiano@icomp.ufam.edu.br>
2025-06-20 04:31:04 -0300

    Adição de arquivos

---
+++ script.py
@@ -0,0 +1,10 @@
+
+def somar(num1, num2):
+    return (num1 + num2)
+
+if __name__ == "__main__":
+    # variaveis
+    a = 5
+    b = 8
+    s = somar(a, b) # funcao para somar
+    print(f'Resultado da soma: {s}')
